a6d72fb90200c0d2965831285b3b3ef59084e1bd
Cin316 <joe13reich@gmail.com>
2013-07-22 15:29:30 -0400

    Created MineZWeapons class.

---
+++ src/com/cin316/minezweapons/MineZWeapons.java
@@ -0,0 +1,63 @@
+package com.cin316.minezweapons;
+
+import java.util.logging.Logger;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.cin316.minezweapons.MineZWeapons;
+
+public class MineZWeapons extends JavaPlugin{
+	
+	public static MineZWeapons plugin;
+	public Logger log;
+	PluginDescriptionFile pdfFile;
+	public String[] helpText;
+	
+	public void onEnable(){
+		log = Logger.getLogger("Minecraft");
+		pdfFile = this.getDescription();
+		helpText = new String[]{
+			ChatColor.YELLOW + pdfFile.getName() + " version " + pdfFile.getVersion(),
+			ChatColor.YELLOW + pdfFile.getDescription()
+		};
+		
+		log.info(pdfFile.getName() + " " + pdfFile.getVersion() + " has been enabled.");
+		
+	}
+	
+	public void onDisable(){
+		log.info(pdfFile.getName() + " " + " has been disabled.");
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+		
+		
+		if( commandLabel.equalsIgnoreCase("minezweapons") ){
+			
+			if( !(sender instanceof Player) ){ //If the console sends the command.
+				ConsoleCommandSender console = (ConsoleCommandSender) sender;
+				
+				console.sendMessage(helpText);
+				
+			}else{ //If a player sends the command.
+				Player player = (Player) sender;
+				
+				if(player.hasPermission("minezweapons.info")){ //If the player has permission.
+					player.sendMessage(helpText);
+				}else{ //If the player doesn't has permission.
+					
+				}
+				
+			}
+			
+		}
+		
+		return true;
+	}
+	
+}
