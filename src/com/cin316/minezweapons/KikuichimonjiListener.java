526b4fc54f472406b182afbbbfe3aef61f1eb0d0
Cin316 <joe13reich@gmail.com>
2013-07-22 16:38:38 -0400

    Added Kikuichimonji.

--- src/com/cin316/minezweapons/KikuichimonjiListener.java
+++ src/com/cin316/minezweapons/KikuichimonjiListener.java
@@ -1,8 +1,16 @@
 package com.cin316.minezweapons;
 
+import java.util.Random;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import com.cin316.minezweapons.MineZWeapons;
 
@@ -17,6 +25,48 @@ public class KikuichimonjiListener implements Listener{
 	@EventHandler
 	public void onEntityDamage(EntityDamageEvent event) {
 	    
+		Entity entity = event.getEntity();
+		//Check if the entity getting hit is a player
+		if (entity instanceof Player){
+			
+			//Create the variable for the player (getting hit)
+			Player hurted = (Player)entity;
+			//Check if the event is a Entity damage event (basically)
+			if (event instanceof EntityDamageByEntityEvent){
+				
+				//Get a variable for the cause of the damage
+				EntityDamageByEntityEvent damageCause = (EntityDamageByEntityEvent)event;
+				//Check if the damager is a player
+				if (damageCause.getDamager() instanceof Player){
+					
+					//Get the variable for the player who punched someone
+					Player hitter = (Player)damageCause.getDamager();
+					
+					//Check if the hitter is holding a Kikuichimonji and it is a wood sword.
+					if( hitter.getItemInHand().getItemMeta().getDisplayName().equals("\u00a7oKikuichimonji") && hitter.getItemInHand().getType().equals(Material.WOOD_SWORD) ){
+						
+						//Generate a random number between 1 and 3.
+						Random rand = new Random();
+						int n = rand.nextInt(3) + 1;
+						Random rand2 = new Random();
+						int n2 = rand.nextInt(3) + 1;
+						
+						if(n==1){
+							//Poison the hurted.
+							hurted.addPotionEffect(new PotionEffect(PotionEffectType.POISON, 1, 60) );
+						}
+						if(n2==1){
+							//Poison the hurter
+							hurted.addPotionEffect(new PotionEffect(PotionEffectType.POISON, 1, 60) );
+						}
+						
+					}
+					
+				}
+				
+			}
+			
+		}
 		
 		
 	}
--- src/com/cin316/minezweapons/MineZWeapons.java
+++ src/com/cin316/minezweapons/MineZWeapons.java
@@ -3,10 +3,13 @@ package com.cin316.minezweapons;
 import java.util.logging.Logger;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -45,20 +48,42 @@ public class MineZWeapons extends JavaPlugin{
 		
 		if( commandLabel.equalsIgnoreCase("minezweapons") ){
 			
-			if( !(sender instanceof Player) ){ //If the console sends the command.
-				ConsoleCommandSender console = (ConsoleCommandSender) sender;
-				
-				console.sendMessage(helpText);
-				
-			}else{ //If a player sends the command.
-				Player player = (Player) sender;
-				
-				if(player.hasPermission("minezweapons.info")){ //If the player has permission.
-					player.sendMessage(helpText);
-				}else{ //If the player doesn't has permission.
+			if( args[0].equalsIgnoreCase("help") ){
+				if( !(sender instanceof Player) ){ //If the console sends the command.
+					ConsoleCommandSender console = (ConsoleCommandSender) sender;
+					
+					console.sendMessage(helpText);
+					
+				}else{ //If a player sends the command.
+					Player player = (Player) sender;
+					
+					if(player.hasPermission("minezweapons.info")){ //If the player has permission.
+						player.sendMessage(helpText);
+					}else{ //If the player doesn't has permission.
+						
+					}
 					
 				}
-				
+			}else if( args[0].equalsIgnoreCase("kikuichimonji") ){
+				if( !(sender instanceof Player) ){ //If the console sends the command.
+					ConsoleCommandSender console = (ConsoleCommandSender) sender;
+					
+					console.sendMessage("You must be a player to use this command.");
+					
+				}else{ //If a player sends the command.
+					Player player = (Player) sender;
+					
+					if(player.hasPermission("minezweapons.use")){ //If the player has permission.
+						ItemStack is = new ItemStack(Material.WOOD_SWORD, 1); //Make a stack of 1 Wood Sword
+						ItemMeta im = is.getItemMeta();
+						im.setDisplayName("\u00a7oKikuichimonji"); //Set its name to KiKuichimonji.
+						is.setItemMeta(im);
+						player.getInventory().addItem(is);
+					}else{ //If the player doesn't has permission.
+						
+					}
+					
+				}
 			}
 			
 		}
