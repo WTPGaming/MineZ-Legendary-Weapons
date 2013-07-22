1a408a930f99ed961f0ba945d9a51e1d6a0a9672
Cin316 <joe13reich@gmail.com>
2013-07-22 15:44:27 -0400

    Added Plugin Manager and connection to KikuichimonjiListener.

--- src/com/cin316/minezweapons/MineZWeapons.java
+++ src/com/cin316/minezweapons/MineZWeapons.java
@@ -1,14 +1,17 @@
 package com.cin316.minezweapons;
 
 import java.util.logging.Logger;
+
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.cin316.minezweapons.KikuichimonjiListener;
 import com.cin316.minezweapons.MineZWeapons;
 
 public class MineZWeapons extends JavaPlugin{
@@ -16,11 +19,13 @@ public class MineZWeapons extends JavaPlugin{
 	public static MineZWeapons plugin;
 	public Logger log;
 	PluginDescriptionFile pdfFile;
+	PluginManager pluginManager;
 	public String[] helpText;
 	
 	public void onEnable(){
 		log = Logger.getLogger("Minecraft");
 		pdfFile = this.getDescription();
+		pluginManager = this.getServer().getPluginManager();
 		helpText = new String[]{
 			ChatColor.YELLOW + pdfFile.getName() + " version " + pdfFile.getVersion(),
 			ChatColor.YELLOW + pdfFile.getDescription()
@@ -28,6 +33,7 @@ public class MineZWeapons extends JavaPlugin{
 		
 		log.info(pdfFile.getName() + " " + pdfFile.getVersion() + " has been enabled.");
 		
+		pluginManager.registerEvents( new KikuichimonjiListener(this), this );
 	}
 	
 	public void onDisable(){
