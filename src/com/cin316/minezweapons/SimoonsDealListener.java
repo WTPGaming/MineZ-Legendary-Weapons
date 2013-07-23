c156dfaea37c708bcd55c3f5c180bbd20659d324
Cin316 <joe13reich@gmail.com>
2013-07-23 13:49:28 -0400

    Added Simoon's Deal implementation.

--- src/com/cin316/minezweapons/SimoonsDealListener.java
+++ src/com/cin316/minezweapons/SimoonsDealListener.java
@@ -1,15 +1,11 @@
 package com.cin316.minezweapons;
 
-import java.util.Random;
-
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
@@ -22,42 +18,30 @@ public class SimoonsDealListener implements Listener {
 	}
 	
 	@EventHandler
-	public void onEntityDamage(EntityDamageEvent event) {
-	    
-		Entity entity = event.getEntity();
-		//Check if the entity getting hit is a player
-		if (entity instanceof Player){
-			
-			//Create the variable for the player (getting hit)
-			Player hurted = (Player)entity;
-			//Check if the event is a Entity damage event (basically)
-			if (event instanceof EntityDamageByEntityEvent){
-				
-				//Get a variable for the cause of the damage
-				EntityDamageByEntityEvent damageCause = (EntityDamageByEntityEvent)event;
-				//Check if the damager is a player
-				if (damageCause.getDamager() instanceof Player){
-					
-					//Get the variable for the player who punched someone
-					Player hitter = (Player)damageCause.getDamager();
-					
-					//Check if the hitter is holding a Kikuichimonji and it is a wood sword.
-					if( hitter.getItemInHand().getType().equals(Material.WOOD_SWORD) ){
-						if( hitter.getItemInHand().getItemMeta().hasDisplayName() ){
-							if( hitter.getItemInHand().getItemMeta().getDisplayName().equals(ChatColor.ITALIC + "Simoon's Deal") ){
-								
-								//Do stuff.
-								
-							}
-						}
-					}
-					
-				}
-				
-			}
-			
-		}
+	public void onPlayerUse(PlayerInteractEvent event) {
 		
+		Player hitter = event.getPlayer();
+		
+		//Check if the hitter is holding a Simoon's Deal and it is a wood sword.
+		if( hitter.getItemInHand().getType().equals(Material.WOOD_SWORD) ){
+			if( hitter.getItemInHand().getItemMeta().hasDisplayName() ){
+				if( hitter.getItemInHand().getItemMeta().getDisplayName().equals(ChatColor.ITALIC + "Simoon's Deal") ){
+					
+					//Do stuff.
+					
+					//Gives the hitter Speed II for 60 seconds.
+					hitter.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 1200, 2) );
+					//Gives the hitter Strength I for 60 seconds.
+					hitter.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 1200, 1) );
+					
+					//Schedules SimoonsDealTask to be run in 60 seconds.
+					new SimoonsDealTask(plugin, hitter).runTaskLater(plugin, 1200);
+					
+					//Destroys Simoon's Deal.
+					hitter.getInventory().setItemInHand(null);
+				}
+			}
+		}
 		
 	}
 	
--- src/com/cin316/minezweapons/SimoonsDealTask.java
+++ src/com/cin316/minezweapons/SimoonsDealTask.java
@@ -1,16 +1,30 @@
 package com.cin316.minezweapons;
 
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class SimoonsDealTask extends BukkitRunnable {
 	
 	MineZWeapons plugin;
+	Player player;
 	
-	public SimoonsDealTask(MineZWeapons plugin) {
-		
+	public SimoonsDealTask(MineZWeapons plugin, Player player) {
+		this.plugin = plugin;
+		this.player = player;
 	}
 	
 	public void run() {
+		
+		//Strikes the player with lightning.
+		player.getWorld().strikeLightning(player.getLocation());
+		//Gives the hitter Nausea I for 60 seconds.
+		player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 1200, 1) );
+		//Gives the hitter Weakness I for 60 seconds.
+		player.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS, 1200, 1) );
+		//Gives the hitter Slowness I for 60 seconds.
+		player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 1200, 1) );
 		
 	}
 
